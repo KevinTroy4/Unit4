57627e2c20a1ee4e6445fd01e7d737305f3e5378
s287339 <s287339@shhac150M05.ocsb.local>
2022-05-13 12:08:56 -0400

    Q1Volume.py complete

--- Q1Volume.py
+++ Q1Volume.py
@@ -1,1 +1,26 @@
-print('hello my cutie')
+# NAME OF AUTHOR:  Kevin
+# NAME OF THE PROGRAM:  Q1Volume.py
+# DATE OF CREATION:  05-11-2022
+# PURPOSE OF PROGRAM:  The purpose of this program is to learn how to code using variables, input commands, etc. 
+
+# VARIABLE DEFINITION
+Lenght = 0
+width = 0
+height = 0
+volume = 0
+
+
+
+
+# INPUT
+length = int(input("enter length"))
+
+width = int(input("enter width"))
+#width = int(input())
+
+height = int(input("enter height"))
+
+print("the volume of the prism is")
+volume = int(input(length * height * width))
+
+print("Have a good day")
