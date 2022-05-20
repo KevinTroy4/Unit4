bcaaba44eb132b09b438db8e4c6d97496811dd4a
s287339 <s287339@shhac150M05.ocsb.local>
2022-05-20 12:12:01 -0400

    finished product

---
+++ Q2RandomRange.py
@@ -0,0 +1,12 @@
+import random
+print("welcome the the random number generator")
+
+
+bob=int(input("input starting number"))
+foo=int(input("input ending number"))
+
+
+
+print(random.randint(bob, foo))
+
+print("thank you for using the random number generator")
